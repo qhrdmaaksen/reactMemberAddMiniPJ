8314e04cecb818e9a4b21258c9f8bea9c1916d40
vitamin777 <rlaalsdn8@gmail.com>
2022-07-20 23:40:01 +0900

    refactor: Backdrop function & ModalOverlay function add, use portal
    -index.html backdrop_root, overlay_root 추가
    -createPortal 사용하여 backdrop, modalOverlay 컴포넌트 세분화하여 리팩토링
    -Button.js 에서 onclick attr add

--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -2,7 +2,7 @@ import React from 'react';
 import classes from './Button.module.css'
 const Button = (props) => {
 	return (
-			<button className={classes.button} type={props.type || 'button'}>
+			<button className={classes.button} type={props.type || 'button'} onClick={props.onClickCloseBTN}>
 				{props.children}
 			</button>
 	)
--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,23 +1,40 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import classes from './ErrorModal.module.css'
 import Card from "./Card";
 import Button from "./Button";
 
-const ErrorModal = (props) => {
+const Backdrop = (props) => {
 	return (
 			<div className={classes.backdrop} onClick={props.onClickCloseBTN}>
-				<Card className={classes.modal}>
-					<header className={classes.header}>
-						<h2>{props.title}</h2>
-					</header>
-					<div className={classes.content}>
-						<p>{props.message}</p>
-					</div>
-					<footer className={classes.actions}>
-						<Button onClick={props.onClickCloseBTN}>닫기</Button>
-					</footer>
-				</Card>
 			</div>
+	)
+}
+const ModalOverlay = (props) => {
+	return (
+			<Card className={classes.modal}>
+				<header className={classes.header}>
+					<h2>{props.title}</h2>
+				</header>
+				<div className={classes.content}>
+					<p>{props.message}</p>
+				</div>
+				<footer className={classes.actions}>
+					<Button onClickCloseBTN={props.onClickCloseBTN}>닫기</Button>
+				</footer>
+			</Card>
+	)
+}
+const ErrorModal = (props) => {
+	//어디에 렌더링할건지
+	const backDropRoot = document.getElementById('backdrop_root');
+	const modalOverlayRoot = document.getElementById('overlay_root');
+	return (
+			<React.Fragment>
+				{ReactDOM.createPortal(<Backdrop onClickCloseBTN={props.onClickCloseBTN}/>, backDropRoot)}
+				{ReactDOM.createPortal(<ModalOverlay title={props.title} message={props.message}
+																						 onClickCloseBTN={props.onClickCloseBTN}/>, modalOverlayRoot)}
+			</React.Fragment>
 	)
 }
 export default ErrorModal;
