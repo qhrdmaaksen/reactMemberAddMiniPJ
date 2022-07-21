e13e5cd35d5aa5e6cd0f047f3036038d045e480c
vitamin777 <rlaalsdn8@gmail.com>
2022-07-21 17:27:41 +0900

    refactor : useState 를 useRef 로 변경 (회원 정보 (이름,나이) 인풋)
    -useRef 사용하여 useState 사용했던 로직 변경
    -회원 정보 입력 시 값만 읽으면되기에 ref 사용했으며 코드 간결 및 속도 개선
    -입력 초기화는 dom 에 접근하여 current.value 로 초기화 해줬음
    -입력 시 정상 출력 확인, 오류 메시지 기능 정상 출력확인

--- src/components/Members/AddMember.js
+++ src/components/Members/AddMember.js
@@ -1,64 +1,76 @@
-import React, {useState} from 'react';
+import React, {useState, useRef} from 'react';
 import Card from '../UI/Card'
 import Button from '../UI/Button'
 import classes from './AddMember.module.css'
 import ErrorModal from "../UI/ErrorModal";
 
 const AddMember = (props) => {
-	const [enteredMemberName, setEnteredMemberName] = useState('')
-	const [enteredMemberAge, setEnteredMemberAge] = useState('')
+	const nameInputRef = useRef()
+	const ageInputRef = useRef()
+	/*const [enteredMemberName, setEnteredMemberName] = useState('')
+	const [enteredMemberAge, setEnteredMemberAge] = useState('')*/
 	const [errorModalPopup, setErrorModalPopup] = useState();
 
 	const addMemberHandler = (event) => {
 		event.preventDefault(); // 새로 고침됐을때 기본 값 초기화 방지
-		if (enteredMemberName.trim().length === 0) {
+		const enteredName = nameInputRef.current.value;
+		const enteredAge = ageInputRef.current.value;
+		console.log(`nameInputRef::: `+nameInputRef)
+		console.log(`ageInputRef::: `+ageInputRef)
+		if (enteredName.trim().length === 0) {
 			setErrorModalPopup({
 				title: '오류 메세지',
 				message: '이름을 입력해주세요.',
 			})
 			return;
-		} else if (enteredMemberAge.trim().length === 0) {
+		} else if (enteredAge.trim().length === 0) {
 			setErrorModalPopup({
 				title: '오류 메세지',
 				message: '나이를 입력해주세요.',
 			})
-			return ;
+			return;
 		}
-		if (+enteredMemberAge < 1) {
+		if (+enteredAge < 1) {
 			return setErrorModalPopup({
 				title: '오류 메세지',
 				message: '0 보다 큰 올바른 나이를 입력해주세요',
 			})
 		}
-		console.log('회원 정보 로그 :: ' + enteredMemberName, enteredMemberAge);
-		props.onAddMember(enteredMemberName, enteredMemberAge);
-		setEnteredMemberName('');
-		setEnteredMemberAge('');
+		/*console.log('회원 정보 로그 :: ' + enteredMemberName, enteredMemberAge);*/
+		props.onAddMember(enteredName, enteredAge);
+		/*setEnteredMemberName('');
+		setEnteredMemberAge('');*/
+		nameInputRef.current.value = '';
+		ageInputRef.current.value = '';
 	}
 	const onClickCloseBTN = () => {
 		setErrorModalPopup(null);
 	}
-	const memberNameChangeHandler = (event) => {
+	/*const memberNameChangeHandler = (event) => {
 		setEnteredMemberName(event.target.value);
 	}
 	const memberAgeChangeHandler = (event) => {
 		setEnteredMemberAge(event.target.value);
-	}
+	}*/
 	return (
 			<React.Fragment>
-					{errorModalPopup && <ErrorModal onClickCloseBTN={onClickCloseBTN}
-											 title={errorModalPopup.title}
-											 message={errorModalPopup.message}
-					/>}
-					<Card className={classes.input}>
-						<form action="" onSubmit={addMemberHandler}>
-							<label htmlFor="memberName">회원 성함</label>
-							<input id="memberName" type="text" value={enteredMemberName} onChange={memberNameChangeHandler}/>
-							<label htmlFor="memberAge">나이</label>
-							<input id="memberAge" type="text" value={enteredMemberAge} onChange={memberAgeChangeHandler}/>
-							<Button type="submit">회원 추가</Button>
-						</form>
-					</Card>
+				{errorModalPopup && <ErrorModal onClickCloseBTN={onClickCloseBTN}
+																				title={errorModalPopup.title}
+																				message={errorModalPopup.message}
+				/>}
+				<Card className={classes.input}>
+					<form action="" onSubmit={addMemberHandler}>
+						<label htmlFor="memberName">회원 성함</label>
+						{/*<input id="memberName" type="text" value={enteredMemberName} ref={nameInputRef}
+									 onChange={memberNameChangeHandler}/>*/}
+						<input id="memberName" type="text" ref={nameInputRef}/>
+						<label htmlFor="memberAge">나이</label>
+						{/*<input id="memberAge" type="text" value={enteredMemberAge} ref={ageInputRef}
+									 onChange={memberAgeChangeHandler}/>*/}
+						<input id="memberAge" type="text" ref={ageInputRef}/>
+						<Button type="submit">회원 추가</Button>
+					</form>
+				</Card>
 			</React.Fragment>
 	)
 }
